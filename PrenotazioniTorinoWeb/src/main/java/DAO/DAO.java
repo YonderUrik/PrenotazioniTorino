c682e515347e91b7d73307041d244067bc69989c
daniele986 <serafino319@gmail.com>
2021-11-19 11:21:52 +0100

    add vue pt.1

---
+++ PrenotazioniTorinoWeb/src/main/java/DAO/DAO.java
@@ -0,0 +1,49 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+public class DAO {
+    private static final String url1 = "jdbc:mysql://localhost:3306/prenotazionitorino";
+    private static final String user = "root";
+    private static final String password = "";
+
+    public static void  registerDriver() {
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            System.out.println("Driver correttamente registrato");
+        } catch (SQLException e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<utente> getUtente(String email, String u_password){
+        Connection conn1 = null;
+        ArrayList<utente> out = new ArrayList<>();
+        try {
+            conn1 = DriverManager.getConnection(url1, user, password);
+
+
+            Statement st1 = conn1.createStatement();
+            ResultSet rs1 = st1.executeQuery("SELECT * FROM utente WHERE email='"+email+"' AND password='"+u_password+"'");
+            while (rs1.next()) {
+                utente u = new utente(rs1.getString("cognome"),rs1.getString("nome"),rs1.getInt("id"),rs1.getString("email"),rs1.getString("password"),rs1.getString("ruolo"));
+                out.add(u);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
+        return out;
+
+    }
+}
---
+++ PrenotazioniTorinoWeb/src/main/java/com/example/prenotazionitorinoweb/AutServlet.java
@@ -0,0 +1,90 @@
+package com.example.prenotazionitorinoweb;
+
+
+import DAO.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "autservlet", value = "/aut-servlet")
+public class AutServlet extends HttpServlet {
+    public void init() {
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+
+        ServletContext ctx = getServletContext();
+        RequestDispatcher rd = ctx.getRequestDispatcher("/index.html");
+
+        String email = request.getParameter("utente");
+        String password = request.getParameter("password");
+        String sessionID = request.getParameter("sessione");
+        HttpSession s = request.getSession(); //estraggo il session ID
+        String jsessionID = s.getId();
+        System.out.println("JSessionID:" + jsessionID);
+        System.out.println("sessionID ricevuto:" + sessionID);
+        System.out.println("email ricevuto:" + email);
+        System.out.println("password ricevuta:" + password);
+
+        if (email == null) {
+            //BISOGNA INSERIRE UNA MAIL VALIDA
+        }
+        if (Objects.equals(email, "guest") && sessionID!=null && jsessionID.equals(sessionID)){
+            //AVVIO LA SESSIONE PER GUEST
+            String role = "guest";
+            s.setAttribute("email",email);
+            s.setAttribute("ruolo", role);
+            //CAMBIO
+        }
+        if (sessionID!=null && jsessionID.equals(sessionID)) {
+            //VERIFICO L'UTENTE
+            ArrayList<utente> utente= DAO.getUtente(email,password);
+            if(utente.get(0).getEmail().equals(email) && utente.get(0).getPassword().equals(password)){
+                String role = utente.get(0).getRuolo();
+                s.setAttribute("email", email);
+                s.setAttribute("ruolo", role);
+                //CAMBIO
+            }
+
+        } else {
+            //SI E' VERIFICATO UN ERRORE ANOMALO, RIPROVA
+        }
+
+    }
+
+
+
+
+
+
+    private String getRole(String username, String password){
+        String temp= null;
+        ArrayList<utente> utente= DAO.getUtente(username,password);
+        if(utente.isEmpty()){
+            return temp;
+        }
+        if(Objects.equals(utente.get(0).getEmail(),username) && Objects.equals(utente.get(0).getPassword(),password)){
+            temp=utente.get(0).getRuolo();
+        }
+
+        return temp;
+    }
+
+}
