c2229f5d0cb5917a7b7a36552cf88216fd8ec09f
daniele986 <serafino319@gmail.com>
2022-03-28 15:36:10 +0200

    ad app android

---
+++ PrenotazoniTorinoApp/app/src/main/java/com/example/prenotazonitorinoapp/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.prenotazonitorinoapp;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.android.volley.Cache;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.textclassifier.TextLinks;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.prenotazonitorinoapp.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    EditText emailBox, passwordBox,nome, cognome;
+    Button  loginbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        emailBox = (EditText)findViewById(R.id.username);
+        passwordBox = (EditText)findViewById(R.id.password);
+        loginbutton = (Button)findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue queue=Volley.newRequestQueue(MainActivity.this);
+                String sessione2="sessione";
+                String URL = "http://192.168.60.120:8080/PrenotazioniTorinoWeb_war_exploded/aut-servlet";
+                StringRequest request=new StringRequest(Request.Method.POST, URL,  new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject respObj = new JSONObject(response);
+                            String utente = respObj.getString(emailBox.getText().toString());
+                            String password = respObj.getString(passwordBox.getText().toString());
+                            String sessione = respObj.getString(sessione2);
+                        } catch (JSONException e) {
+                            System.out.println("valore"+response);
+                            e.printStackTrace();
+                        }
+
+                        System.out.println("valore " + response);
+                        try {
+                            JSONObject temp= new JSONObject(response);
+                            System.out.println("oggetto: "+temp.get("ruolo"));
+                            if(temp.get("ruolo").equals("admin") || temp.get("ruolo").equals("studente") ){
+                                binding = ActivityMainBinding.inflate(getLayoutInflater());
+                                setContentView(binding.getRoot());
+
+                                BottomNavigationView navView = findViewById(R.id.nav_view);
+                                // Passing each menu ID as a set of Ids because each
+                                // menu should be considered as top level destinations.
+                                AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                                        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                                        .build();
+                                NavController navController = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_activity_main);
+                                NavigationUI.setupActionBarWithNavController(MainActivity.this, navController, appBarConfiguration);
+                                NavigationUI.setupWithNavController(binding.navView, navController);
+                            }else if(temp.get("ruolo").equals("ospite")){
+                                setContentView(R.layout.guest);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+
+                        @Override
+                        protected Map<String, String> getParams() {
+                        // below line we are creating a map for
+                        // storing our values in key and value pair.
+                        Map<String, String> params = new HashMap<String, String>();
+
+                        // on below line we are passing our key
+                        // and value pair to our parameters.
+                        params.put("utente",emailBox.getText().toString());
+                        params.put("password", passwordBox.getText().toString());
+                        params.put("sessione", "password2");
+
+
+                        // at last we are
+                        // returning our params.
+                        return params;
+                    }
+
+                };
+
+
+                queue.add(request);;
+
+            }
+        });
+
+
+    }
+
+
+}
